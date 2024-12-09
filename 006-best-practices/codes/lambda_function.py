6a84746ba030240ee94829471dd9cd9fe46467a3
kennedy-ak <akogokennedy@gmail.com>
2024-12-09 11:01:42 +0000

    chapter-6

--- 006-best-practices/codes/lambda_function.py
+++ 006-best-practices/codes/lambda_function.py
@@ -5,19 +5,19 @@ import base64
 
 import mlflow
 
-kinesis_client = boto3.client('kinesis')
+# kinesis_client = boto3.client('kinesis')
 
-PREDICTIONS_STREAM_NAME = os.getenv('PREDICTIONS_STREAM_NAME', 'ride_predictions')
-
-
-RUN_ID = os.getenv('RUN_ID')
-
-logged_model = f's3://mlflow-models-alexey/1/{RUN_ID}/artifacts/model'
-# logged_model = f'runs:/{RUN_ID}/model'
-model = mlflow.pyfunc.load_model(logged_model)
+# PREDICTIONS_STREAM_NAME = os.getenv('PREDICTIONS_STREAM_NAME', 'ride_predictions')
 
 
-TEST_RUN = os.getenv('TEST_RUN', 'False') == 'True'
+# RUN_ID = os.getenv('RUN_ID')
+
+# logged_model = f's3://mlflow-models-alexey/1/{RUN_ID}/artifacts/model'
+# # logged_model = f'runs:/{RUN_ID}/model'
+# model = mlflow.pyfunc.load_model(logged_model)
+
+
+# TEST_RUN = os.getenv('TEST_RUN', 'False') == 'True'
 
 def prepare_features(ride):
     features = {}
--- 006-best-practices/codes/test_docker.py
+++ 006-best-practices/codes/test_docker.py
@@ -5,7 +5,7 @@ import json
 import requests
 from deepdiff import DeepDiff
 
-with open('event.json', 'rt', encoding='utf-8') as f_in:
+with open('./event.json', 'rt', encoding='utf-8') as f_in:
     event = json.load(f_in)
 
 
